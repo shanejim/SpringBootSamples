e9f05f19971dab1077f0197519208b9af96cc860
PanShenjia <ShaneJim@users.noreply.github.com>
2018-10-25 17:02:27 +0800

    test控制器修改

--- personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/TestController.java
+++ personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.shanejim.myweb.personaladmin.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author shanejim
  * @description todo
  * @date 2018/10/21
  */
+@RestController
 public class TestController {
+
 
 }
