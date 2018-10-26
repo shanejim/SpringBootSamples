919dcafb027d3daf9dcf4aebd050d06d84c87bba
PanShenjia <ShaneJim@users.noreply.github.com>
2018-10-26 16:46:58 +0800

    后台页面

--- personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/PageController.java
+++ personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/PageController.java
@@ -1,9 +1,32 @@
 package com.shanejim.myweb.personaladmin.controller;
 
+import com.shanejim.myweb.personalmodel.entity.PayMall;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
 /**
  * @description: TODO
  * @author: panshenjia
  * @create: 2018-10-26 15:42
  **/
 public class PageController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+    @GetMapping("/page/payMall/index")
+    public ModelAndView weixinPayMallIndex() {
+
+        ModelAndView mv = new ModelAndView("payMall/PayMallIndex");
+        PayMall payMall = new PayMall();
+        payMall.setMallkey("woshikey123");
+        mv.getModel().put("user", payMall);
+        mv.addObject("time", new Date());
+        return mv;
+    }
 }
