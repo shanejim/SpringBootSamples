14eadf112748ae02742b04dc555171221adebda4
PanShenjia <ShaneJim@users.noreply.github.com>
2018-10-26 16:24:52 +0800

    加个http请求异常类

---
+++ personal-model/src/main/java/com/shanejim/myweb/personalmodel/config/HttpClientRuntimeException.java
@@ -0,0 +1,13 @@
+package com.shanejim.myweb.personalmodel.config;
+
+/**
+ * @description: TODO
+ * @author: panshenjia
+ * @create: 2018-10-26 16:13
+ **/
+public class HttpClientRuntimeException extends RuntimeException {
+    public HttpClientRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
