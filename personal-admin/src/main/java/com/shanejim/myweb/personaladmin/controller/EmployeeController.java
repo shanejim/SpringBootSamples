f230d869e4ef794a20a4cfdc7bfc41c8740b5023
PanShenjia <ShaneJim@users.noreply.github.com>
2018-10-30 17:33:32 +0800

    更新

--- personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/EmployeeController.java
+++ personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.shanejim.myweb.personaladmin.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @description: TODO
  * @author: panshenjia
  * @create: 2018-10-26 11:17
  **/
+@RestController
 public class EmployeeController {
+
 }
+
