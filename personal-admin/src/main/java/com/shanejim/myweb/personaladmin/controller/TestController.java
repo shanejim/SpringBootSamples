9e8962e849da84eb18d70462c1a6563d626581bf
PanShenjia <ShaneJim@users.noreply.github.com>
2018-12-20 22:49:58 +0800

    excel

--- personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/TestController.java
+++ personal-admin/src/main/java/com/shanejim/myweb/personaladmin/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.shanejim.myweb.personaladmin.controller;
 
+import com.shanejim.myweb.personalmodel.entity.PayMall;
 import com.shanejim.myweb.personalmodel.enums.CodeEnums;
 import com.shanejim.myweb.personalmodel.query.TestQuery;
 import com.shanejim.myweb.personalmodel.response.Result;
@@ -7,6 +8,7 @@ import com.shanejim.myweb.personalmodel.response.UploadReturn;
 import com.shanejim.myweb.personalmodel.utils.FileUtil;
 import com.shanejim.myweb.personalmodel.utils.ResultUtil;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.xssf.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -17,12 +19,16 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import redis.clients.jedis.JedisPool;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author shanejim
@@ -112,6 +118,56 @@ public class TestController {
         return ResultUtil.success();
     }
 
+    @GetMapping("/UserExcelDownloads")
+    public void downloadAllClassmate(HttpServletResponse response) throws IOException {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        List<PayMall> classmateList = new ArrayList<>();
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        String fileName = "userinf" + ".xlsx";//设置要导出的文件的名字
+
+
+        XSSFSheet sheet = workbook.createSheet("信息表");
+        //新增数据行，并且设置单元格数据
+
+        int rowNum = 1;
+
+        for (int i = 0; i < 70000; i++) {
+            PayMall chatRoom = new PayMall();
+            chatRoom.setAliAppid("名称123");
+            chatRoom.setAliAppPrivateKey("名称333");
+            chatRoom.setAliAppPublicKey("名称555");
+            chatRoom.setHostUrl("关键词");
+            classmateList.add(chatRoom);
+        }
+
+        String[] headers = {"名称", "用户个数", "到期时间", "关键词"};
+        //headers表示excel表中第一行的表头
+
+        XSSFRow row = sheet.createRow(0);
+        //在excel表中添加表头
+
+        for (int i = 0; i < headers.length; i++) {
+            XSSFCell cell = row.createCell(i);
+            XSSFRichTextString text = new XSSFRichTextString(headers[i]);
+            cell.setCellValue(text);
+        }
+
+        //在表中存放查询到的数据放入对应的列
+        for (PayMall teacher : classmateList) {
+            XSSFRow row1 = sheet.createRow(rowNum);
+            row1.createCell(0).setCellValue(teacher.getAliAppid());
+            row1.createCell(1).setCellValue(teacher.getAliAppPrivateKey());
+            row1.createCell(2).setCellValue(teacher.getAliAppPrivateKey());
+            row1.createCell(3).setCellValue(teacher.getAliPublicKey());
+            rowNum++;
+        }
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+        response.flushBuffer();
+        workbook.write(response.getOutputStream());
+    }
 
     //分布式锁
       /*  Jedis jedis = RedisUtil.getConn();
