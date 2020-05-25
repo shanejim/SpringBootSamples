0177c4dada166676d52575fceb197c97141555ef
shane <1693831790@qq.com>
2020-05-25 23:23:23 +0800

    config swagger ui

---
+++ src/main/java/com/shanejim/github/config/Swagger2Config.java
@@ -0,0 +1,42 @@
+package com.shanejim.github.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @description: TODO
+ * @author: panshenjia
+ * @create: 2018-10-26 14:13
+ **/
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shanejim.github.controller")) //需要注意的是这里要写入控制器所在的包
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("shanejim", "https://github.com/ShaneJim", "panshenjia@126.com");
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风格，https://swagger.io/")
+                .termsOfServiceUrl("https://swagger.io/")
+                .contact(contact)
+                .version("1.0")
+                .build();
+    }
+}
